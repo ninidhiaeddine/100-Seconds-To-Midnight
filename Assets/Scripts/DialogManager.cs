552224bd951ec88c9a48732c35de108201dfbea8
Dhia Eddine Nini <dxn00@mail.aub.edu>
2022-04-20 22:20:41 +0300

    Finished Dialogue System Infrastructure

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,41 +1,84 @@
 using UnityEngine;
 using System.Collections;
 
-enum Character { Character1, Character2 };
-
 public class DialogManager : MonoBehaviour
 {
+    // singleton:
+    public static DialogManager Instance { get; private set; }
+
+    // dialog:
     public DialogScriptableObject dialog;
 
-    IEnumerator StartDialog()
+    void Awake()
     {
-        int i = 0;
-        DialogLineDurationPair lineDurationPair;
-
-        while (true)
+        if (Instance != null && Instance != this)
         {
-            if (i % 2 == 0)
-                lineDurationPair = GetLineDurationPair(Character.Character1, i);
-            else
-                lineDurationPair = GetLineDurationPair(Character.Character2, i);
-
-            // increment:
-            i++;
-
-            yield return lineDurationPair.line;
-            yield return new WaitForSeconds(lineDurationPair.duration);
-
-            // TODO: IndexOutOfBounds exception will occur here. Needs fixing
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
         }
     }
 
-    DialogLineDurationPair GetLineDurationPair(Character character, int index)
+    private void Start()
     {
-        if (character == Character.Character1)
-            return dialog.character1LinesDurationPairs[index];
-        else if (character == Character.Character2)
-            return dialog.character2LinesDurationPairs[index];
+        StartCoroutine(StartDialog());
+    }
+
+    public IEnumerator StartDialog()
+    {
+        DialogLineDurationPair[] interleavedDialogLines;
+        if (dialog.startWith == Character.Character1)
+        {
+            interleavedDialogLines = InterleaveArrays<DialogLineDurationPair>(
+                dialog.character1LinesDurationPairs, 
+                dialog.character2LinesDurationPairs
+                );
+        }
         else
-            throw new System.Exception("Unexpected value");
+        {
+            interleavedDialogLines = InterleaveArrays<DialogLineDurationPair>(
+                dialog.character2LinesDurationPairs,
+                dialog.character1LinesDurationPairs
+                );
+        }
+
+        for (int i = 0; i < interleavedDialogLines.Length; i++)
+        {
+            Debug.Log(interleavedDialogLines[i].line);
+            yield return new WaitForSeconds(interleavedDialogLines[i].duration);
+        }
+    }
+
+    // helper functions:
+
+    private T[] InterleaveArrays<T>(T[] array1, T[] array2)
+    {
+        T[] result = new T[array1.Length + array2.Length]; 
+        int minLength = Mathf.Min(array1.Length, array2.Length);
+        int maxLength = Mathf.Max(array1.Length, array2.Length);
+
+        int i = 0;
+        while(i < minLength)
+        {
+            result[i * 2] = array1[i];
+            result[(i * 2) + 1] = array2[i];
+
+            i++;
+        }
+
+        int j = i * 2;
+        while (i < maxLength)
+        {
+            if (maxLength == array1.Length)
+                result[j] = array1[i];
+            else if(maxLength == array2.Length)
+                result[j] = array2[i];
+
+            i++;
+            j++;
+        }
+        return result;
     }
 }
--- Assets/Scripts/DialogScriptableObject.cs
+++ Assets/Scripts/DialogScriptableObject.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public enum StartWith { Character1, Character2 }
+public enum Character { Character1, Character2 }
 
 [CreateAssetMenu(fileName = "Dialog", menuName = "ScriptableObjects/DialogScriptableObject", order = 1)]
 public class DialogScriptableObject : ScriptableObject
 {
     public DialogLineDurationPair[] character1LinesDurationPairs;
     public DialogLineDurationPair[] character2LinesDurationPairs;
-    public StartWith startWith = StartWith.Character1;
+    public Character startWith = Character.Character1;
 }
